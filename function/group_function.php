953c3b63ab467037d879fc5ab1a5e45e9c10f589
00galaxy-jing <donghae1206@163.com>
2015-05-10 15:47:04 +0800

    兴趣组详情界面

---
+++ function/group_function.php
@@ -0,0 +1,69 @@
+<?php
+    $version = "1.3.3b";
+    //$maxRows = 30;
+    //$tasklevel = 0;
+    mysql_select_db($database_tankdb,$tankdb);
+
+    if (!function_exists("GetSQLValueString")) {
+    function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+    {
+      if (PHP_VERSION < 6) {
+        $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+      }
+
+      $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+
+      switch ($theType) {
+        case "text":
+          $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+          break;    
+        case "long":
+        case "int":
+          $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+          break;
+        case "double":
+          $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
+          break;
+        case "date":
+          $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+          break;
+        case "defined":
+          $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+          break;
+      }
+      return $theValue;
+    }
+}
+
+//获取全部热门推荐
+function get_group_info($g_id){
+
+  global $tankdb;
+
+  $selGroupInfo="SELECT * FROM gt_group WHERE group_id=$g_id";
+  $GroupInfoRS = mysql_query($selGroupInfo, $tankdb) or die(mysql_error());
+  return $GroupInfoRS;
+}
+
+//获取问题信息
+function get_pro_info($g_id){
+
+  global $tankdb;
+
+  $selProInfo="SELECT * FROM gt_problem WHERE problem_group=$g_id";
+  $ProInfoRS = mysql_query($selProInfo, $tankdb) or die(mysql_error());
+  return $ProInfoRS;
+}
+
+//获取回答信息
+function get_answer_info($pro_id){
+
+  global $tankdb;
+
+  $selAnswerInfo="SELECT * FROM gt_answer,gt_teacher WHERE answer_pid=$pro_id AND answer_point_status=1 AND answer_user=tea_id";
+  $AnswerInfoRS = mysql_query($selAnswerInfo, $tankdb) or die(mysql_error());
+  $AnswerInfo = mysql_fetch_assoc($AnswerInfoRS);
+  return $AnswerInfo;
+}
+
+?>
---
+++ group_view.php
@@ -0,0 +1,158 @@
+<?php require_once('config/db_config.php'); ?>
+<?php require_once('session/session_unset.php'); ?>
+<?php require_once('session/session.php'); ?>
+<?php require_once('function/group_function.php'); ?>
+
+<!--变量初始化部分-->
+<?php 
+	$now_groupid=-1;
+	if(isset($_GET['gid'])){
+  		$now_groupid = $_GET['gid'];
+	}
+
+	$now_uid = 1;
+?>
+
+<!--数据库操作部分-->
+<?php 
+	$group_infoRS = get_group_info($now_groupid);
+	$group_info = mysql_fetch_array($group_infoRS);
+	$problem_infoRS = get_pro_info($now_groupid);
+?> 
+
+<?xml version="1.0" encoding="UTF-8"?>
+
+<!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.0//EN" "http://www.wapforum.org/DTD/xhtml-mobile10.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head> 
+	<?php require('header.php');?>
+	<script language="javascript">
+		if (top.location != location) top.location.href = location.href;
+	</script>
+</head>
+
+<body>	
+	<div style="height:40px"data-role="header" data-position="fixed" data-fullscreen="false" class="header" id="iheader" data-theme="a">
+	      <h3>交大好老师</h3>
+    </div>
+
+	<link rel="stylesheet" href="css/bbslist.css?v=2013013007" type="text/css" />
+	
+	<div id="main">
+		<div class="bbsdata_list">
+			<table width="100%">
+					<tr>
+						<td style="width:80px;height:100%" > 
+							<img src="<?php echo $group_info['group_pic']; ?>" width="80px"  >
+						</td>
+						<td>
+							<dl>
+								<dt>
+									<p style="font-size:150%;font-weight:bold;color:#3C3B3B">[<?php echo $group_info['group_name']; ?>]</p>
+									<p class="clear"></p>
+								</dt>
+								<dd class="bbsdata_info">
+									<p style="font-size:120%"><?php echo $group_info['group_description']; ?></p>
+									<p><span >老师：<?php echo $group_info['group_tnum']; ?></span>&nbsp&nbsp&nbsp&nbsp&nbsp<span style="color:rgb(82, 179, 202);">查看所有老师</span></p>				
+									<span >感兴趣：<?php echo $group_info['group_snum']; ?></span>			
+								</dd>
+							</dl>
+						</td>
+					</tr>
+
+			</table>
+			<div style="width: 100%;
+							padding: 1px 0px;
+								border-bottom: 1px solid #dcdcdc;">
+			</div>
+
+			<!--列表开始-->
+			<?php while($row_pro = mysql_fetch_assoc($problem_infoRS)) {?>
+			<div style="border-left:1px solid rgb(219, 219, 219);border-right:1px solid rgb(219, 219, 219);">
+				<table width="100%" >
+					<tr>
+						<td style="padding-bottom:0px"  colspan="2" >
+						<!--<img src="images/shouwei/jobs/my.jpg" width="70%">-->
+							<dl style="padding:3px">
+								<dt>
+									<p style="font-size:15px;color:rgb(8, 150, 211);font-weight:bold"><?php echo $row_pro['problem_title']; ?></p>
+								</dt>
+								<dd>
+									<p style="text-align:left"><span>发起时间：</span>
+									<span><?php echo $row_pro['problem_time']; ?></span></p>
+									<?php if($row_pro['problem_point_status']==1) {?>
+										<p style="text-align:left">--------------教师回复-------------</p>
+									<?php }?>
+								</dd>
+							</dl>			
+						</td>			
+					</tr>				
+									<!--<font class="rpy two_num fl">19</font>-->
+									<!--<p style="font-size:150%;font-weight:bold;color:#3C3B3B">[学术组]</p>
+									<p class="clear"></p>-->
+				    <?php if($row_pro['problem_point_status']==1) {
+				    	       $pro_id=$row_pro['problem_id'];
+				    	       $answer_info = get_answer_info($pro_id);
+				     ?>
+						<tr>
+							<!--<td style="padding-top:0px;padding-bottom:3px;width:30%" >
+								<img src="images/shouwei/jobs/my.jpg">
+							</td>-->
+							<td style="width:50px;height:100%;padding-top:0px;padding-bottom:3px;vertical-align:top;"  > 
+								<img src="<?php echo $answer_info['tea_pic']; ?>" width="50px" >
+							</td>
+							<td  style="height:100%;padding-top:0px;padding-bottom:3px;font-size:13px;color:#898989;text-align:left;vertical-align:top">
+								<dd>
+									<p style="color:rgb(33, 177, 219)"><?php echo $answer_info['tea_name']; ?></p>
+										<p><?php echo $answer_info['answer_content']; ?></p>		
+								</dd>
+							</td>	
+						</tr>
+					<?php }?>
+				</table>	
+			</div>
+
+				
+				<div style="width: 100%;
+							padding: 1px 0px;
+								border-bottom: 1px solid #dcdcdc;">
+				</div>
+
+				<?php } ?>
+
+		</div>
+	</div>
+
+	<div id="foot">
+		<!--<a class="font13  mr12 c64" title="电脑版" href="../www.paidai.com/?id=1_2F">电脑版</a>
+		<a class="font13  mr12 c64" title="触屏版" href="shouji">触屏版</a>
+        		<a href="login.html" title="登录" class="font13 mr12 c64">登&nbsp;录</a>-->
+			<p style="font-size: 9px;text-align:center">Copyright ©2015 BJTU</p>
+	</div>
+
+<!-- 底下的固定菜单栏-->
+    <div data-position="fixed" data-role="footer" data-id="footernav">
+      <div data-role="navbar" data-position="fixed">
+        <ul>
+          <li>
+            <a href="home.php" data-icon="home"   data-theme="a">动态</a>
+          </li>
+          <li>
+            <a href="hot_recom.php" rel="external" data-icon="star" data-theme="a">发现</a>
+          </li>
+          <li>
+            <a href="question.php" data-icon="edit" data-theme="a">提问</a>
+          </li>
+          <li>
+            <a href="me.html" data-icon="user" data-theme="a">我</a>
+          </li>
+          <li>
+            <a data-icon="bars" data-theme="a">更多</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+</body>
+</html> 
