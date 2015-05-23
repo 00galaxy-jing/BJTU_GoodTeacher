59500721e352e55ccd90504d85e6a776dfb8911b
vivianwangyu <vivianwangyu1110@gmail.com>
2015-05-23 10:23:14 +0800

    老师的个人主页

---
+++ function/teacher_function.php
@@ -0,0 +1,46 @@
+<?php
+    $version = "1.3.3b";
+    mysql_select_db($database_tankdb,$tankdb);
+
+    if (!function_exists("GetSQLValueString")) {
+    function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+    {
+      if (PHP_VERSION < 6) {
+        $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+      }
+
+      $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+
+      switch ($theType) {
+        case "text":
+          $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+          break;    
+        case "long":
+        case "int":
+          $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+          break;
+        case "double":
+          $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
+          break;
+        case "date":
+          $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+          break;
+        case "defined":
+          $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+          break;
+      }
+      return $theValue;
+    }
+}
+
+//获取老师信息
+function get_teacher_info($uid){
+  global $tankdb;
+
+  $selTeaInfo="SELECT * FROM gt_teacher WHERE tea_id=$uid";
+  $TeaRS = mysql_query($selTeaInfo, $tankdb) or die(mysql_error());
+  $tea_info = mysql_fetch_assoc($TeaRS);
+
+  return $tea_info;
+}
+?>
---
+++ me_teacher.php
@@ -0,0 +1,124 @@
+<?php require_once('config/db_config.php'); ?>
+<?php require_once('session/session_unset.php'); ?>
+<?php require_once('session/session.php'); ?>
+<?php require_once('function/teacher_function.php'); ?>
+<?php require_once('function/message_function.php'); ?>
+
+<?php 
+  //初始化
+  $tea_id = -1;
+  if(isset($_GET['tid'])){
+        $tea_id = $_GET['tid'];
+    }
+  $mes = -1;
+  if(isset($_GET['mes'])){
+        $mes = $_GET['mes'];
+        set_read($mes);
+    }
+  $now_uid=$_SESSION['MM_uid'];
+  $now_type=$_SESSION['MM_role'];
+
+  //调用function
+  $user_info = get_teacher_info($tea_id);
+?>
+<?xml version="1.0" encoding="UTF-8"?>
+
+<!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.0//EN" "http://www.wapforum.org/DTD/xhtml-mobile10.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+  <?php require('header.php'); ?>
+</head>
+
+<body>
+  <script type="text/javascript">
+      function get_data()
+      {
+        $.ajax({
+          url: 'getMessage.php',
+          success: function(data) {
+            document.getElementById('more_m').innerHTML=data;
+          }
+        });
+      }
+
+      setInterval("get_data()",3000);//1秒一次执行
+    </script> 
+  <!-- Page: me  -->
+  <div id="me" data-role="page">
+    <div data-role="header" data-position="fixed" class="header" id="mheader" data-theme="a">
+      <h3>交大好老师</h3>
+    </div>
+    <div role="main" class="ui-content">
+      <!-- 个人信息 -->
+        <div class="imgtest" style="text-align:center;">
+          <div style="font-family: cursive;font-size: 20px;"><?php echo $user_info['tea_name']; ?></div>
+          <figure style="  margin-top: 5px;margin-bottom:5px">
+            <div>
+              <img src="<?php echo $user_info['tea_pic']; ?>" />
+            </div>  
+          </figure>
+        </div>
+        <p>所属院系：<?php echo $user_info['tea_major'] ?></p>
+        <p>邮箱：<?php echo $user_info['tea_mail'] ?></p>
+        <p>电话：<?php echo $user_info['tea_tel'] ?></p>
+        <p>个人简介：<?php echo $user_info['tea_intro'] ?></p>
+       </div>
+    <div data-position="fixed" data-role="footer">
+      <div data-role="navbar">
+        <ul>
+          <li>
+            <a href="home.php" data-icon="home" data-theme="a">动态</a>
+          </li>
+          <li>
+            <a href="hot_recom.php" data-icon="star" data-theme="a">发现</a>
+          </li>
+          <li>
+            <a href="question.php" data-icon="edit" data-theme="a">提问</a>
+          </li>
+          <li>
+            <a href="me_student.php?sid=<?php echo $now_uid ?>" data-icon="user" data-theme="a" class="ui-btn-active">我</a>
+          </li>
+          <li>
+            <a data-icon="bars" data-theme="a" id="more_m">更多</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+
+  <!-- 圆形头像style   margin:10px 5px; -->
+  <style type="text/css">
+    .imgtest{
+
+    overflow:hidden;
+    }
+    .list_ul figcaption p{
+    font-size:12px;
+    color:#aaa;
+    }
+    .imgtest figure div{
+    display:inline-block;
+    margin:0px auto;
+    width:100px;
+    height:100px;
+    border-radius:100px;
+    border:2px solid #fff;
+    overflow:hidden;
+    -webkit-box-shadow:0 0 3px #ccc;
+    box-shadow:0 0 3px #ccc;
+    }
+    .imgtest img{width:100%;
+    min-height:100%; text-align:center;
+    }
+    .ui-content p{
+        margin: 3px;
+        font-family: "微软雅黑";
+        text-align:center;
+    }
+  </style>
+</body>
+
+</html>
